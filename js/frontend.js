6f70223d2943ded7369ddc3ec1a7f7bdf16af392
Mauro Jurada <37582356+MauroJurada@users.noreply.github.com>
2020-06-12 19:24:18 +0200

    added loading stuff

--- js/frontend.js
+++ js/frontend.js
@@ -2,20 +2,20 @@
 var query = {
     userType: false, //true means Verified, false means all
     userName: "user1", //array of selected user names, or just one username for now?
-    dateFrom : "2020-01-01" , //unix time TODO
-    dateTo : "2020-01-05", //CHANGE THIS!
+    dateFrom: "2020-01-01", //unix time TODO
+    dateTo: "2020-01-05", //CHANGE THIS!
     //colorPallete : "pallete1", //name of the collor pallete 
-    minFontSize : 20,
-    maxFontSize : 80,
-    maxTags : 50,
-    color : "#696969",
-    rotation : 0,
+    minFontSize: 20,
+    maxFontSize: 80,
+    maxTags: 50,
+    color: "#696969",
+    rotation: 0,
     font: "Impact", //add more font names in index.html as options
     words: [],
     loading: ""
 };
 
-function setUserType(value){
+function setUserType(value) {
     if (value == 1) {
         query.userType = true;
     } else {
@@ -23,39 +23,39 @@ function setUserType(value){
     }
 }
 
-function setUserName(value){
+function setUserName(value) {
     query.userName = value;
 }
 
-function setDateFrom(value){
+function setDateFrom(value) {
     query.dateFrom = value;
 }
 
-function setDateTo(value){
+function setDateTo(value) {
     query.dateTo = value;
 }
 
-function setFont(value){
+function setFont(value) {
     query.font = value;
 }
 
-function setColor(value){
+function setColor(value) {
     query.color = value;
 }
 
-function setMinFontSize(value){
+function setMinFontSize(value) {
     query.mainFontSize = value;
 }
 
-function setMaxFontSize(value){
+function setMaxFontSize(value) {
     query.maxFontSize = value;
 }
 
-function setMaxTags(value){
+function setMaxTags(value) {
     query.maxTags = value;
 }
 
-function setRotation(value){
+function setRotation(value) {
     query.rotation = value;
 }
 
@@ -73,51 +73,60 @@ function showMenu(menuChoice) {
 }
 
 function generate() {
+    document.getElementById("generateButton").innerHTML = "Loading";
+
+    fetchMeta(loadingBarStart);
     document.getElementById("my_dataviz").innerHTML = "";
-    //ToDo: this should refresh the:  my_dataviz div
-    fetchMeta(loadingBar);
+
     //drawTagCloud();
-  //  console.log(query.loading);
-    //console.dir(query);
+
+    //ToDo: make sure this is called only when the tag cloud is actually drawn
+    loadingBarSTOP();
 }
 
-function fetchMeta(_callback){
+function fetchMeta(_callback) {
     var url = "http://127.0.0.1:5000/meta";
     var request = new XMLHttpRequest()
     request.open('GET', url);
     request.responseType = 'text';
     request.send();
-    request.onload = function() {
+    request.onload = function () {
         var responseArray = request.response;
-        responseArray = responseArray.slice( 1, -1);  
+        responseArray = responseArray.slice(1, -1);
         query.loading = responseArray.replace(/"/g, "");
         _callback();
     };
 }
 
-function loadingBar(){
-    console.log(query.loading);
+function loadingBarStart() {
+    document.getElementById("loadingIcon").style.display = "block";
+    document.getElementById("loadingText").innerHTML = query.loading;
 }
 
-function fetchData(){
+function loadingBarSTOP() {
+    document.getElementById("loadingIcon").style.display = "none";
+    document.getElementById("generateButton").innerHTML = "Generate";
+}
+
+function fetchData() {
     var url = "http://127.0.0.1:5000/searchWords";
     var request = new XMLHttpRequest()
     request.open('GET', url);
     request.responseType = 'text';
     request.send();
-    request.onload = function() {
+    request.onload = function () {
         var responseArray = request.response;
         responseArray = responseArray.replace(/\s+/g, '');
         responseArray = responseArray.replace(/'/g, '');
         var myArray = new Array();
-        myArray = responseArray.slice( 1, -1).split(",");
+        myArray = responseArray.slice(1, -1).split(",");
         var i;
         var listOfObjects = [];
-        for (i = 0; i < myArray.length; i+=2) {
-          var singleObj = {}
-          singleObj['word'] = myArray[i];
-          singleObj['size'] = parseInt(myArray[i+1], 10);
-          listOfObjects.push(singleObj);
+        for (i = 0; i < myArray.length; i += 2) {
+            var singleObj = {}
+            singleObj['word'] = myArray[i];
+            singleObj['size'] = parseInt(myArray[i + 1], 10);
+            listOfObjects.push(singleObj);
         }
         query.words = listOfObjects;
     };
